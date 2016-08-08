ea11bc1f7e7f193e67d9ea40dd0d18d63920436b
masamorishita <info@masamorishita.com>
2016-08-08 02:00:01 +0100

    Updated a lot

---
+++ Assets/Scripts/Launch.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Launch : MonoBehaviour {
+	void Start()
+	{
+		Invoke("Call",5);
+	}
+	void Call()
+	{
+		SceneManager.LoadScene("Login");
+	}
+}
